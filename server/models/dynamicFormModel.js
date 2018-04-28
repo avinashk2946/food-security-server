d508d6314610f4ff983e92d72ee91274b3ec15b7
avinashk2946 <38369609+avinashk2946@users.noreply.github.com>
2018-04-28 16:59:40 +0530

    Dynamic Form API : DB Schema modified

--- server/models/dynamicFormModel.js
+++ server/models/dynamicFormModel.js
@@ -3,10 +3,16 @@ const mongoose = require('mongoose'),
     constants = require("./../../config/constants"),
     Schema = mongoose.Schema,
     dynamicFormSchema = new mongoose.Schema({
-        formTypeId: {type: Schema.Types.ObjectId, ref: 'formtypes'},
+        formTypeId: {type: Schema.Types.ObjectId, ref: 'formtypes',unique: true},
+        unitTypeId: {type: Schema.Types.ObjectId, ref: 'unittypes'},
+        companyId: {type: Schema.Types.ObjectId, ref: 'companies'},
         labels: [String],
         formMetaData: Schema.Types.Mixed,
-        createdDate: { type: Date, default: new Date() }
+        status: { type: Boolean, default: false },
+        createdDate: { type: Date, default: new Date() },
+        modifiedDate: { type: Date, default: new Date() },
+        createdBy: { type: Schema.Types.ObjectId, ref: 'users' },
+        modifiedBy: { type: Schema.Types.ObjectId, ref: 'users' }
     });
 dynamicFormSchema.plugin(uniqueValidator, { message: constants.messages.error.templateExists });
 const dynamicFormModel = mongoose.model('dynamicForm', dynamicFormSchema);
